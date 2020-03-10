c51a669e5a647891af84c298dcdc863d1ca6325e
dmarzzz <danmarzec@icloud.com>
2020-03-09 20:59:53 -0400

    create-react-app

---
+++ web3-example/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Ethereum Address: 
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
